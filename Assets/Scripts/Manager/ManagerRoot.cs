ac2a24585999228e8fadac643f75dffff15575e6
KimJongWeon <dhdlqns12@gmail.com>
2025-10-30 16:29:13 +0900

    feat: managerRoot및 UIBase추가
    매니저들을 중앙집중적으로 관리하기 위해 managerRoot를 추가하였습니다. UI들을 상속받게 하기 위해 UIBase를 추가 하였습니다.

---
+++ Assets/Scripts/Manager/ManagerRoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class ManagerRoot : Singleton<ManagerRoot>
+{
+    [Header("직접 참조")]
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private UIManager uiManager;
+
+    public static GameManager GameManager { get; private set; }
+    public static UIManager UIManager { get; private set; }
+
+    protected override void Init()
+    {
+        GameManager = gameManager;
+        UIManager = uiManager;
+    }
+}
