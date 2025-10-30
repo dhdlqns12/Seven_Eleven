8fe4692df7e35359f6414669dd61dc3523e95c0c
Gayeong-you <yko66901@gmail.com>
2025-10-30 19:09:20 +0900

    chore: 플레이어 이동 스크립트(수정필요)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,27 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
-public class PlayerController : MonoBehaviour
+public abstract class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
-    public float jumpForce = 15f;
+    public abstract void HandleAction();
+    
 
-    [Header("컴포넌트 연결")] 
-    [SerializeField]
-    private Animator animator;
-    private Rigidbody2D rb;
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            //위로 이동 =>점프 애니메이션
-        }
-    }
 }
