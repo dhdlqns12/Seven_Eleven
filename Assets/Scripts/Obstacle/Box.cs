a451c0f999553b69fd3a7d5d82df4324336bc3b1
yun <ycy6797@naver.com>
2025-10-30 12:13:51 +0900

    feat:박스 추가 chore:메타 파일 올리기..

---
+++ Assets/Scripts/Obstacle/Box.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MainGameManager.Instance.playerInRange = true;//Player 추가되면 수정
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MainGameManager.Instance.playerInRange = false;//Player 추가되면 수정
+        }
+    }
+}
