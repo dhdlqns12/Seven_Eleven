147b3934c0a5c41ce32e30ffd35bcf25c488639e
Gayeong-you <yko66901@gmail.com>
2025-10-31 11:11:37 +0900

    feat: 플레이어 이동과 점프
    속도값은 맵과 연결 후 테스트 필요합니다.

--- Assets/Scripts/Player/Boy.cs
+++ Assets/Scripts/Player/Boy.cs
@@ -2,27 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boy : PlayerController
+namespace Player
 {
-    public override void HandleAction()
+    public class Boy : PlayerController
     {
-        float horizontalInput = 0f;
-        bool jumpRequested = false;
-        
-        if (Input.GetKey(KeyCode.W))
+        public override void HandleAction()
         {
-            jumpRequested = true;
-        }
-
-        else if (Input.GetKey(KeyCode.A))
-        {
-            horizontalInput = -1f;
-        }
-
-        else if (Input.GetKey(KeyCode.D))
-        {
-            horizontalInput = 1f;
-        }
+            float horizontalInput = 0f;
+            float verticalInput = 0f;
+            jumpRequsted = false;
             
+            if (Input.GetKey(KeyCode.W))
+            {
+                jumpRequsted = true;
+            }
+
+            else if (Input.GetKey(KeyCode.A))
+            {
+                horizontalInput = -1f;
+                spriteRenderer.flipX = true;
+            }
+
+            else if (Input.GetKey(KeyCode.D))
+            {
+                horizontalInput = 1f;
+                spriteRenderer.flipX = false;
+            }
+            
+            movementDirection=new Vector2(horizontalInput, verticalInput).normalized;
+            
+        }
     }
+
 }
