ea042ef946e760088a267f4c85f67066630de07f
yun <ycy6797@naver.com>
2025-11-02 23:55:49 +0900

    Revert "fix:스위치 정상작동 확인 및 파일정리"
    This reverts commit 000ed93bed2e809f12a26e77b56688b52f3ddde6.

--- Assets/Scripts/Manager/ManagerRoot.cs
+++ Assets/Scripts/Manager/ManagerRoot.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class ManagerRoot : Singleton<ManagerRoot>
 {
-    [Header("직접 참조")]//인스펙터랑 인스턴스를 연결한거
+    [Header("직접 참조")]
     [SerializeField] private GameManager gameManager;
     [SerializeField] private UIManager uiManager;
     [SerializeField] private SceneController sceneController;
     [SerializeField] private AudioManager audioManager;
-    [SerializeField] private SwitchManager switchManager;
 
-    public static GameManager GameManager { get; private set; }//프로퍼티를 활용한 static선언
+    public static GameManager GameManager { get; private set; }
     public static UIManager UIManager { get; private set; }
     public static SceneController SceneController { get; private set; }
     public static AudioManager AudioManager { get; private set; }
 
-    public static SwitchManager SwitchManager { get; private set; }
-
-    protected override void Init()//싱글톤 안에 virtual 메서드가 없는데 이건 왜 있는거??
+    protected override void Init()
     {
-        GameManager = gameManager;//static선언한 변수에 인스펙터에 연결된 인스턴스를 대입
+        GameManager = gameManager;
         UIManager = uiManager;
         SceneController = sceneController;
         AudioManager = audioManager;
-        SwitchManager = switchManager;
     }
 }
---
+++ Assets/Scripts/Obstacle/SpecificTrapZone.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SpecificTrapZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D _player)
+    {
+        if (_player.CompareTag("Fire")|| _player.CompareTag("Water"))
+        {
+            ManagerRoot.GameManager.isCollisionObstacle = true;
+
+            //물이 용암 충돌=>사망
+            //불이 물 충돌=>사망
+            //물이 얼음 충돌=>느려짐 collision.getcomponent<스크립트이름>
+            //불이 얼음 충돌=>빨라짐
+            //물이 식물 충돌=> 식물이 자라나는 애니메이션+밟고 있던 바닥이 발판이 되서 위로 이동함
+            //엘레베이터를 작동시킴>>이중딕셔너리 
+            //물,불,박스는 스위치를 누를 수 있음 
+            //코인 먹고 점수 처리//jason?
+            
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D _player)
+    {
+        if (_player.CompareTag("Player"))
+        {
+
+            //스위치를 충돌영역을 벗어나면 스위치가 다시 원위치로 올라감
+
+            ManagerRoot.GameManager.isCollisionObstacle = false;
+        }
+    }
+}
