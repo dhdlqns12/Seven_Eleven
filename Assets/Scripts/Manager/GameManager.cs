a20cb16c763982f86fbfe9100a5d2498a18d8800
KimJongWeon <dhdlqns12@gmail.com>
2025-11-04 15:27:35 +0900

    build: 빌드용 세팅

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.InputSystem.Utilities;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
